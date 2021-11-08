56a927b1de8d54149f853eb7751abbbc72bfe95d
gotmjkim <goatmjkim@gamil.com>
2021-11-08 16:38:46 +0900

    ff test..

--- packages/mfe2/config/webpack.prod.js
+++ packages/mfe2/config/webpack.prod.js
@@ -2,12 +2,13 @@ const { merge } =require('webpack-merge');
 const ModuleFedarationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common.js')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const prodConfig ={
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/mfe1/latest/',
+        publicPath: '/mfe2/latest/',
     },
     plugins:[
         new ModuleFedarationPlugin({
